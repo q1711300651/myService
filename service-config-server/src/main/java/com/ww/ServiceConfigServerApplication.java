f7c3307fcbe2196abaec0e0d115addad3ab74759
ww <1711300651@qq.com>
2017-08-31 09:14:24 +0800

    add config-server

---
+++ service-config-server/src/main/java/com/ww/ServiceConfigServerApplication.java
@@ -0,0 +1,12 @@
+package com.ww;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceConfigServerApplication.class, args);
+	}
+}
